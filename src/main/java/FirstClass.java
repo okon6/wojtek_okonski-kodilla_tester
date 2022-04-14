b286df600eaa02c895492d03dc0928df15717bbf
Wojtek Okonski <okonski.wojtek@gmail.com>
2022-04-14 22:01:26 +0200

    proba 1

---
+++ src/main/java/FirstClass.java
@@ -0,0 +1,9 @@
+public class FirstClass {                           // [1]
+    public static void main(String[] args) {         // [2]
+        System.out.println("Hello World!");            // [3]
+        System.out.println("Wojtek");//4
+        System.out.println(35);                          //5
+        System.out.println('O');//6
+
+    }
+}
