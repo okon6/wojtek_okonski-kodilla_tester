98cbb98b965bf22e18bb856668b9d7fb5c24e0da
Wojtek Okonski <okonski.wojtek@gmail.com>
2022-04-19 22:16:41 +0200

    Initial version

---
+++ kodilla-intro/src/main/java/HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+        System.out.println("Wojtek");
+        System.out.println(34);
+        System.out.println('O');
+
+    }
+}
