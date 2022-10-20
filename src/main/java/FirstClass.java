83e3634af127f40cc92ad856c46246fe3f89936d
Wojtek Okonski <okonski.wojtek@gmail.com>
2022-10-20 17:09:19 +0200

    Initial version

--- src/main/java/FirstClass.java
+++ src/main/java/FirstClass.java
@@ -1,9 +1,12 @@
+import static java.lang.System.out;
+
 public class FirstClass {                           // [1]
     public static void main(String[] args) {         // [2]
-        System.out.println("Hello World!");            // [3]
-        System.out.println("Wojtek");//4
-        System.out.println(35);                          //5
-        System.out.println('O');//6
+        out.println("Hello World!");            // [3]
+        out.println("Wojtek");
+        out.println(35);
+        out.println('O');
+
 
     }
 }
