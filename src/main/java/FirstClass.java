a4cbf6e7533cae269be6af5ebe40bbb6109620b3
Wojtek Okonski <okonski.wojtek@gmail.com>
2022-10-22 16:03:40 +0200

    Initial version

--- src/main/java/FirstClass.java
+++ src/main/java/FirstClass.java
@@ -1,12 +1,17 @@
-import static java.lang.System.out;
+public class FirstClass {
+    public static void main(String[] args) {
 
-public class FirstClass {                           // [1]
-    public static void main(String[] args) {         // [2]
-        out.println("Hello World!");            // [3]
-        out.println("Wojtek");
-        out.println(35);
-        out.println('O');
+        Notebook notebook = new Notebook(600, 1000);
+        System.out.println(notebook.weight + " " + notebook.price);
+        notebook.checkPrice();
 
+        Notebook heavyNotebook = new Notebook(2000, 1500);
+        System.out.println(heavyNotebook.weight + " " + heavyNotebook.price);
+        heavyNotebook.checkPrice();
+
+        Notebook oldNotebook = new Notebook(1600, 500);
+        System.out.println(oldNotebook.weight + " " + oldNotebook.price);
+        oldNotebook.checkPrice();
 
     }
 }
